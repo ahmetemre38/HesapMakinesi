6f397a6bad23fbf8d6578100762bcbd3abd1662c
= <=>
2021-02-24 21:15:21 +0200

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,33 +1,41 @@
 const readline = require("readline");
-//const { isNumber } = require("util");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 var sayi1,sayi2;
 
-rl.question("ilk sayi giriniz : ", (s1) => {
-
-    sayi1=parseInt(s1); 
-
-    if (!isNumber(sayi1))
+rl.question("ilk sayıyı giriniz : ", (s1) => {
+    sayi1 = Number(s1);    
+    if (isNaN(sayi1) || sayi1==null)
     {
-        console.log(`Sayi girisi yapmadiniz!!! `);
+        console.log(`Sayı girişi yapmadınız!!! `);
         rl.close();
     }
-    else{
-        rl.question("ikinci sayi giriniz : ", (s2) => {
-            console.log(`sayi1 : ${sayi1}, sayi2 : ${s2}`);
-            rl.close();
+    else {        
+        rl.question("İşlemi giriniz (+,-,*,/): ", (islem) => {
+            if (!((islem=='+') || (islem=='-') || (islem=='*') || (islem=='/')))
+            {
+                console.log(`Geçerli işlem girişi yapmadınız! (+,-,*,/)`);
+                rl.close();                
+            }
+            else
+            {
+                rl.question("ikinci sayıyı giriniz : ", (s2) => {
+                    sayi2 = Number(s2);
+                    if (isNaN(sayi2) || sayi2==null)
+                    {
+                        console.log(`Sayı girişi yapmadınız!!!`);
+                        rl.close();                
+                    }
+                    else
+                    {
+                        console.log(`${sayi1} ${islem} ${sayi2}`);
+                        rl.close();
+                    }
+                });
+            }
         });
-    }
+    }    
 });
 
-/*
-rl.question("What is your name? ", function(name) {
-    rl.question("Where do you live? ", function(country) {
-        console.log(`${name}, is a citizen of ${country}`);
-        rl.close();
-    });
-});
-*/
