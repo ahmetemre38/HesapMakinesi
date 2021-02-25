1b1508b48393625848cbca64e862350881b33533
= <=>
2021-02-25 13:56:54 +0200

    son commit

---
+++ calc_function.js
@@ -0,0 +1,42 @@
+const calc = (num1, operator, num2) => {
+    let number1,number2;
+    let calcResult = { result:0,errors:[] }; 
+    number1 = Number(num1);
+    number2 = Number(num2);
+    if (isNaN(number1) || number1==null)
+    {
+        calcResult.result = 'Hesaplanamadı';
+        calcResult.errors.push(`ilk parametre sayı girilmedi!!!`);
+    }
+    else if (isNaN(number2) || number2==null)
+    {
+        calcResult.result = 'Hesaplanamadı';
+        calcResult.errors.push(`ikinci parametre sayı girilmedi!!!`);
+    }
+    else
+        switch (operator) {
+            case '+':
+                calcResult.result = number1 + number2;            
+                break;
+            case '-':
+                calcResult.result = number1 - number2;
+                break;
+            case '*':
+                calcResult.result = number1 * number2;
+                break;
+            case '/':
+                if (number2 == 0) {
+                    calcResult.errors.push('Sıfıra bölme hatası yapıldı!!!');
+                    calcResult.result = 'Tanımsız';
+                }
+                else calcResult.result = number1 / number2;
+                break;
+            default:
+                calcResult.result = 'Hesaplanamadı';
+                calcResult.errors.push('Operator yanlış girildi!!!')
+                break;        
+        }
+    return calcResult;    
+}
+
+console.log(calc('a','+','1'));
